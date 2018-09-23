97a022fa47de71372827b84d1d16fc5e4a52fb93
yijinxie <jin1xie@163.com>
2018-09-23 14:54:08 +0800

    First commit.

---
+++ libs/searcher.py
@@ -0,0 +1,15 @@
+# coding=UTF-8
+# /*************************************************
+# Copyright:
+# Author:
+# Date:
+# Description: 信息的爬取搜索。参考模块：http://docs.pyspider.org/en/latest/tutorial/
+# **************************************************/
+
+
+def debug(*args, **kwarg):
+    pass
+
+
+if __name__ == '__main__':
+    pass
